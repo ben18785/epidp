f7b1bc36224942340984c30f1039c00077131f93
ben18785 <ben.c.lambert@gmail.com>
2024-10-03 17:54:42 +0100

    added in real covid-19 case data + vignette

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Daily COVID-19 case data for Bogota and Medellin and corresponding Google mobility data
+#'
+#' @format ## `covid_colombia_cases_deaths_mobility`
+#' A data frame with 1348 rows and 10 columns:
+#' \describe{
+#'   \item{date}{Daily}
+#'   \item{city}{Bogota or Medellin}
+#'   \item{cases}{Daily counts of COVID-19 cases}
+#'   \item{deaths}{Daily counts of COVID-19 deaths}
+#'   ...
+#' }
+#' @source <https://github.com/TRACE-LAC/pet-epi-notebooks/tree/main>
+#' @source <https://www.google.com/covid19/mobility/>
+"covid_colombia_cases_deaths_mobility"
