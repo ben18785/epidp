441876fbb617c2b8015ab97332897f6608ca23f3
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-10-15 18:22:55 +0200

    Fix note about unused imports

---
+++ R/epidp-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel  RcppParallelLibs
+## usethis namespace: end
+NULL
